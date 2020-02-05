432db541913c4b8b079260a77cd0685aaef7367a
jinchenxiangdan <jinchenxiangdan@gmail.com>
2020-02-05 16:35:26 -0700

    added reading command option

--- src/main/java/org/ualibrary/awss3tool/App.java
+++ src/main/java/org/ualibrary/awss3tool/App.java
@@ -15,6 +15,7 @@ import com.amazonaws.services.s3.model.S3ObjectSummary;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.List;
 import java.io.File;
 import java.nio.file.Paths;
@@ -28,7 +29,7 @@ public class App
 {
     private static final String DELIMITER = "/";
     private static final String DEFAULT_FOLDER = "";
-    private static AmazonS3 S3;
+
     private static final String ORDERS = "0: List all objects.\n" +
             "1: List file & folders in current file.\n" +
             "2: Upload file/folder (Usage: 2 [path to file/folder] [target position])" +
@@ -36,6 +37,8 @@ public class App
             "cd: Get in folder" +
             "q: Quit.";
 
+    private static AmazonS3 S3;
+    private static HashMap<String, Bucket> BUCKETS_MAP;
 
     public static void main( String[] args )
     {
@@ -77,6 +80,38 @@ public class App
                     listObjects(currentBucketName, prefix);
                     break;
                 case "2":
+                    // check the file path is valid or not
+                    int type = fileChecker(commands[1]);
+                    if (type == -1) {
+                        System.out.println("Error: Invalid path.");
+                        break;
+                    }
+                    // check the target bucket is valid or not
+                    if (!BUCKETS_MAP.containsKey(commands[2])) {
+                        System.out.println("Error: Invalid bucket name.");
+                        break;
+                    }
+                    // upload
+                    StringBuilder targetPath = new StringBuilder();
+                    targetPath.append("s3://");
+                    targetPath.append(currentBucketName);
+                    targetPath.append(prefix);
+
+                    if (type == 1) {            // upload file
+                        System.out.println(targetPath.toString());
+                        if (!uploadFolder(commands[1], targetPath.toString())) {
+                            System.err.println("Upload failed!");
+                            break;
+                        }
+
+                    } else if (type == 2) {     // upload folder
+                        if (!uploadFile(commands[1], targetPath.toString())) {
+                            System.err.println("Upload failed!");
+                            break;
+                        }
+                    }
+                    break;
+
 
 
 
@@ -94,32 +129,31 @@ public class App
         System.exit(0);
 
 
-        // check folder or file
+    }
 
-        System.out.println("Enter file/folder path");
-        String input = scanner.next();
+    /**
+     * Check the path is point to a file or folder
+     * @param input a path in string
+     * @return -1: invalid path
+     *          1: is directory
+     *          2: is regular file
+     */
+    private static int fileChecker(String input) {
         Path file = new File(input).toPath();
         // check if the file exist or not
-        if (Files.exists(file)) {
-
-        } else {
-            // the file doesn't exist
+        if (!Files.exists(file)) {
             System.err.printf("Error: Cannot find file %s.\n", file);
+            return -1;
         }
         // check the path is a file or folder
         if (Files.isDirectory(file)) {
-            System.out.printf("Going to upload folder %s", file);
-
-
+            return 1;
+        } else if (Files.isRegularFile(file)) {
+            return 2;
         } else {
-            // other?
+            System.out.println("Unknown file type");
+            return -1;
         }
-
-
-
-
-
-
     }
 
 
@@ -135,6 +169,7 @@ public class App
         System.out.println("Your Amazon S3 buckets are:");
         for (int i = 0; i < bucketList.size(); i++) {
             System.out.println(i + " - " + bucketList.get(i).getName());
+            BUCKETS_MAP.put(bucketList.get(i).getName(), bucketList.get(i));
         }
         System.out.println("===========================");
         return bucketList;
@@ -143,26 +178,24 @@ public class App
 
     /**
      * upload file to the aws s3
-     * @param s3 AmazonS3 service
      * @param filePath string path to file
      * @param bucketName the bucket name will be upload to
      */
-    private static void uploadFile(AmazonS3 s3, String filePath, String bucketName) {
+    private static boolean uploadFile(String filePath, String bucketName) {
         System.out.format("Uploading %s to S3 bucket %s...\n", filePath, bucketName);
         String keyName = Paths.get(filePath).getFileName().toString();
         try {
 
-            s3.putObject(bucketName, keyName, new File(filePath));
+            S3.putObject(bucketName, keyName, new File(filePath));
 
         } catch (AmazonServiceException e) {
             System.err.println(e.getErrorMessage());
-            System.exit(1);
+            return false;
         }
-
-        System.out.println("Done.");
+        return true;
     }
 
-    private static void uploadFolder(AmazonS3 s3, String filePath, String bucketName) {
+    private static boolean uploadFolder(String filePath, String bucketName) {
         final String scriptPath = "/home/wan/Desktop/ShawnJin_Workspace/awstool/src/main/bash_script/aws_s3_up_folder.sh";
         // formatting filePath?
 
@@ -179,17 +212,23 @@ public class App
             process.waitFor();
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
+            return false;
         }
 
         // get the exit status
-        assert process != null;
+//        assert process != null;
         if (process.exitValue() == 0) {
             System.out.println("Done.");
         } else {
             System.out.println("Something Wrong.");
+            return false;
         }
-        System.out.printf("exist value: %d", process.exitValue());
-
+//        System.out.printf("exist value: %d", process.exitValue());
+        if (process.exitValue() != 0) {
+            return false;
+        } else {
+            return true;
+        }
 
     }
 
